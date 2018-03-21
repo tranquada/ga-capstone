eced9f9716d2f13b700e685b3389404426b1efa4
Tranq <matt.tranquada@gmail.com>
2018-03-21 15:02:21 -0400

    Integrated parse() function into module

--- L1.py
+++ L1.py
@@ -6,13 +6,14 @@ import re            # Python regular expression utilities
 script parsing operations. """
 
 # MODULE VARIABLE DECLARATIONS
-B_OPEN = re.compile(r'<b>')      # RegEx signature for opening bold tag
-B_CLOSE = re.compile(r'</b>')    # RegEx signature for closing bold tag
-TAG = re.compile(r'</?\w+>')     # RegEx signature for html tags in general
-PUNC = re.compile(r'[\!-/<-@\[-`\{-~]')
-CHAR = re.compile(r'\w')         # RegEx signature for characters
-UPPERS = string.ascii_uppercase  # String of all uppercase letters
-LOWERS = string.ascii_lowercase  # String of all lowercase letters
+B_OPEN = re.compile(r'<b>')              # RegEx signature for opening bold tag
+B_CLOSE = re.compile(r'</b>')            # RegEx signature for closing bold tag
+TAG = re.compile(r'</?\w+>')             # RegEx signature for html tags
+PUNC = re.compile(r'[\!-/<-@\[-`\{-~]')  # RegEx signature for punctuation
+NUM = re.compile(r'[0-9]')               # RegEx signature for numbers
+CHAR = re.compile(r'\w')                 # RegEx signature for characters
+UPPERS = string.ascii_uppercase          # String of all uppercase letters
+LOWERS = string.ascii_lowercase          # String of all lowercase letters
 
 
 class Line(object):
@@ -32,11 +33,13 @@ class Line(object):
             'spc': None,
             'rgx': None,
             'pnc': None,
+            'num': None,
         }
 
         # FUNCTION DICTIONARY INITIALIZATION
         self.function = {  # Dictionary to capture line function classification
             'action': None,
+            'blank': None,
             'camera': None,
             'character': None,
             'dialogue': None,
@@ -73,6 +76,44 @@ class Line(object):
         self.metrics['bold'].update(
             {'open': False, 'close': False, 'bolded': None, }
         )
+        self.metrics['space'].update(
+            {'lead': 0}
+        )
 
         # STATUS FLAG INITIALIZATION
         self.state = None  # Flag will cycle between None/parse/json/db
+
+    def parse(self, idx, line):
+        """Parses line information from a raw line string with an index."""
+
+        # 1. BASIC LINE INFORMATION
+        # ---------------------------------------------------------------------
+        # Set data.raw to line data input
+        self.data['raw'] = line
+
+        # Set metrics.id and metrics.length based on input
+        self.metrics['id'] = idx + .1
+        self.metrics['length'] = len(line)
+
+        # Set L1 links information
+        self.links['L1'] = self.metrics['id']
+
+        # 2. EMPTY LINE TEST
+        # ---------------------------------------------------------------------
+        if len(line) == 0:
+            # Set function.empty to True
+            self.function['empty'] = True
+            return
+
+        # 3. BOLD CHECK ROUTINE
+        # ---------------------------------------------------------------------
+        self.parse_bold()
+
+        # 4. CHARACTER PARSING ROUTINE
+        # ---------------------------------------------------------------------
+        self.parse_char()
+
+        # 5. GENERATING REPRESENTATIONAL DATA
+        # ---------------------------------------------------------------------
+        self.data['fmt'] = self.generate_fmt()
+        self.data['rgx'] = self.generate_rgx()
